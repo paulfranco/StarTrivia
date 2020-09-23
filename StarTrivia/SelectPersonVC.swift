aba72f48abbf4727dc96953ddbc9dd86886dc621
Paul Franco <mail@paulfran.co>
2020-09-23 07:26:59 -0700

    created Main Bio

--- StarTrivia/SelectPersonVC.swift
+++ StarTrivia/SelectPersonVC.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class SelectPersonVC: UIViewController {
 
+    @IBOutlet weak var stackBg: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stackBg.layer.cornerRadius = 10
     }
 
 
